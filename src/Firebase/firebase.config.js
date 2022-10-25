0d66ad8d02f5c427e40eaea346c83762e296cf4b
toukircse77 <toukircse777@gmail.com>
2022-10-25 09:56:29 +0600

    create components

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6baxoKXCT7D2EsGzi8LrgM44c6sRoLbk",
+  authDomain: "educamp-45bf5.firebaseapp.com",
+  projectId: "educamp-45bf5",
+  storageBucket: "educamp-45bf5.appspot.com",
+  messagingSenderId: "241041517800",
+  appId: "1:241041517800:web:a906f62cda27f55d0bed5d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
